44eb444b3d1be05f20f989fa0296191a55751369
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-05-09 12:47:32 +0600

    Initial Commit

---
+++ weather_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather_swiftui
+//
+//  Created by Abdur Rahman on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather_swiftui/weather_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_swiftuiApp.swift
+//  weather_swiftui
+//
+//  Created by Abdur Rahman on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
