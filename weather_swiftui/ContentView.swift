8ad33160a5db43024799f3cf4500396894363ba0
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-05-16 15:10:17 +0600

    latest

--- weather_swiftui/ContentView.swift
+++ weather_swiftui/ContentView.swift
@@ -10,16 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            BackgroundView(topColor: .blue, bottomColor: Color.lightBlue)
             
             VStack{
-                Text("Cupertino,CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+           
+                CityTextView(cityName: "Cupertino, CA")
                 
+                MainWeatherStatusView(weatherImage: "cloud.sun.fill", temperature: 76)
+                
+                
+              
+                .padding(.bottom,40)
+            
+                HStack(spacing:20){
+                   WeatherDayView(dayOfWeek: "Tue", imageName: "cloud.sun.fill", temperature: 46)
+                    
+                    WeatherDayView(dayOfWeek: "Wed", imageName: "wind.snow", temperature: 46)
+                    
+                    WeatherDayView(dayOfWeek: "Fri", imageName: "cloud.sun.fill", temperature: 46)
+                    WeatherDayView(dayOfWeek: "Sat", imageName: "sunset.fill", temperature: 46)
+                    WeatherDayView(dayOfWeek: "Sun", imageName: "cloud.sun.rain.fill", temperature: 46)
+                    
+                }
+
                   Spacer()
+                
+                
+                Button(action: {
+                    print("Tapped")
+                }, label: {
+                    WeatherButton(title: "Change Day time", textColor: .blue, backgroundColor: .white)
+                })
+                
+                Spacer()
+                
             }
         }
         
@@ -29,3 +53,78 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    
+    var imageName: String
+    
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+
+        LinearGradient(gradient: Gradient(colors: [topColor,bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView:View {
+    
+    var cityName:String
+    
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var weatherImage:String
+    
+    var temperature : Int
+    
+    var body: some View {
+        VStack(spacing:10){
+            Image(systemName: weatherImage)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium, design: .default))
+                .foregroundColor(.white)
+
+        }
+    }
+}
+
